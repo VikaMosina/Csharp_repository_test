bbd48c914ce404869857ce98e318d602af7b89c4
Mosina Viktoria <vmosina@rit-it.com>
2024-09-02 21:00:39 +0700

    Выделила вспомогательные методы для тестов группы контактов, создала вспомогательный класс для хранения свойств и использовала этот класс в тестах для инициализации тестовых данных и для передачи этих данных во вспомогательные методы. Потренировала создание классов и объектов.

--- Addressbook_web_tests/GroupCreationTests.cs
+++ Addressbook_web_tests/GroupCreationTests.cs
@@ -42,29 +42,66 @@ namespace WebAddressbookTests
         [Test]
         public void GroupCreation()
         {
-            driver.Navigate().GoToUrl(baseURL);
-            driver.FindElement(By.Name("user")).Click();
-            driver.FindElement(By.Name("user")).Clear();
-            driver.FindElement(By.Name("user")).SendKeys("admin");
-            driver.FindElement(By.Name("pass")).Click();
-            driver.FindElement(By.Name("pass")).Clear();
-            driver.FindElement(By.Name("pass")).SendKeys("secret");
-            driver.FindElement(By.XPath("//input[@value='Login']")).Click();
-            driver.FindElement(By.LinkText("groups")).Click();
-            driver.FindElement(By.XPath("//div[@id='content']/form/input[4]")).Click();
-            driver.FindElement(By.Name("group_name")).Click();
-            driver.FindElement(By.Name("group_name")).Clear();
-            driver.FindElement(By.Name("group_name")).SendKeys("a");
-            driver.FindElement(By.Name("group_header")).Click();
-            driver.FindElement(By.Name("group_header")).Clear();
-            driver.FindElement(By.Name("group_header")).SendKeys("b");
-            driver.FindElement(By.Name("group_footer")).Click();
-            driver.FindElement(By.Name("group_footer")).Clear();
-            driver.FindElement(By.Name("group_footer")).SendKeys("c");
-            driver.FindElement(By.Name("submit")).Click();
+            OpenHomePage();
+            Login("admin", "secret");
+            GoToGroupsPage();
+            InitNewGroupCreation();
+            FillGroupForm("a", "b", "c");
+            SubmitGroupCreation();
+            ReturnToGroupsPageAndLogout();
+        }
+         
+        private void ReturnToGroupsPageAndLogout()
+        {
+
             driver.FindElement(By.LinkText("group page")).Click();
             driver.FindElement(By.LinkText("Logout")).Click();
         }
+
+        private void SubmitGroupCreation()
+        {
+            driver.FindElement(By.Name("submit")).Click();
+        }
+
+        private void FillGroupForm(string name, string header, string footer)
+        {
+            driver.FindElement(By.Name("group_name")).SendKeys(name);
+            driver.FindElement(By.Name("group_header")).Click();
+            driver.FindElement(By.Name("group_header")).Clear();
+            driver.FindElement(By.Name("group_header")).SendKeys(header);
+            driver.FindElement(By.Name("group_footer")).Click();
+            driver.FindElement(By.Name("group_footer")).Clear();
+            driver.FindElement(By.Name("group_footer")).SendKeys(footer);
+        }
+
+        private void InitNewGroupCreation()
+        {
+            driver.FindElement(By.Name("group_name")).Clear();
+        }
+
+        private void GoToGroupsPage()
+        {
+            driver.FindElement(By.Name("group_name")).Click();
+        }
+
+        private void Login(string username, string password)
+        {
+            driver.FindElement(By.Name("user")).Click();
+            driver.FindElement(By.Name("user")).Clear();
+            driver.FindElement(By.Name("user")).SendKeys(username);
+            driver.FindElement(By.Name("pass")).Click();
+            driver.FindElement(By.Name("pass")).Clear();
+            driver.FindElement(By.Name("pass")).SendKeys(password);
+            driver.FindElement(By.XPath("//input[@value='Login']")).Click();
+            driver.FindElement(By.LinkText("groups")).Click();
+            driver.FindElement(By.XPath("//div[@id='content']/form/input[4]")).Click();
+        }
+
+        private void OpenHomePage()
+        {
+            driver.Navigate().GoToUrl(baseURL);
+        }
+
         private bool IsElementPresent(By by)
         {
             try
---
+++ Addressbook_web_tests/TestFigure/Circle.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Addressbook_web_tests
+{
+    class Circle : Figure
+    {
+        private int radius; // переменная радиуса
+
+        public Circle(int radius) {
+
+            this.radius = radius;
+
+        }
+        // Свойство "Радиус"
+        public int Radius {
+
+            get // получение
+            {
+                return radius;
+            }
+
+            set // запись
+            {
+                radius = value;
+            }
+
+        }
+    }
+}
